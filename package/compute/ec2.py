1431242fe52b54f7e874216b1b1556a426c52d00
diodonfrost <antoine.mace@protonmail.com>
2019-03-02 03:07:35 +0100

    add paginator in ec2 function

--- package/compute/ec2.py
+++ package/compute/ec2.py
@@ -1,10 +1,15 @@
 
 """This script nuke all ec2 resources"""
 
+import logging
 import time
 import boto3
+from botocore.exceptions import ClientError
 
-EC2 = boto3.client('ec2')
+# Setup simple logging for INFO
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
+
 
 def nuke_all_ec2(older_than_seconds, logger):
     """
@@ -12,44 +17,46 @@ def nuke_all_ec2(older_than_seconds, logger):
         and launchtemplate resources
     """
 
-    #### Nuke all ec2 instances ####
-    # Initialize instance list
-    instance_list = []
-    reservations = EC2.describe_instances()
+    # Define the connection
+    ec2 = boto3.client('ec2')
+    paginator = ec2.get_paginator('describe_instances')
+    page_iterator = paginator.paginate()
     time_delete = time.time() - older_than_seconds
 
+    # Initialize instance list
+    instance_list = []
+
     # list all instances
-    for reservation in reservations['Reservations']:
-        for instance in reservation['Instances']:
+    for page in page_iterator:
+        for reservation in page['Reservations']:
+            for instance in reservation['Instances']:
+                if instance['LaunchTime'].timestamp() < time_delete:
+                    # Retrieve ec2 instance id and add in list
+                    instance_id = instance['InstanceId']
+                    instance_list.insert(0, instance_id)
 
-            if instance['LaunchTime'].timestamp() < time_delete and \
-            instance['State']['Name'] != 'terminated':
+        if instance_list:
+            # Nuke all instances
+            try:
+                ec2.stop_instances(InstanceIds=instance_list)
+                LOGGER.info("Stop instances %s", instance_list)
+            except ClientError:
+                print('No instance found')
 
-                # Retrieve ec2 instance id and add in list
-                instance_id = instance['InstanceId']
-                instance_list.insert(0, instance_id)
-
-    if instance_list:
-        # Nuke all instances
-        EC2.terminate_instances(InstanceIds=instance_list)
-        logger.info("Nuke instances %s", instance_list)
-
-    #### Nuke all launch templates ####
-    response = EC2.describe_launch_templates()
+    # Nuke all launch templates
+    response = ec2.describe_launch_templates()
 
     for launchtemplate in response['LaunchTemplates']:
-
         if launchtemplate['CreateTime'].timestamp() < time_delete:
 
             # Nuke all launch template
-            EC2.delete_launch_template(LaunchTemplateId=launchtemplate['LaunchTemplateId'])
+            ec2.delete_launch_template(LaunchTemplateId=launchtemplate['LaunchTemplateId'])
             logger.info("Nuke Launch Template %s", launchtemplate['LaunchTemplateId'])
 
-    #### Nuke all placement group ####
-    response = EC2.describe_placement_groups()
+    # Nuke all placement group
+    response = ec2.describe_placement_groups()
 
     for placementgroup in response['PlacementGroups']:
-
         # Nuke all launch template
-        EC2.delete_placement_group(GroupName=placementgroup['GroupName'])
+        ec2.delete_placement_group(GroupName=placementgroup['GroupName'])
         logger.info("Nuke Placement Group %s", placementgroup['GroupName'])
