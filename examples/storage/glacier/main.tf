1468f17fd80280509d5ddee6cf3beeb9584964e9
diodonfrost <diodon.frost@diodonfrost.me>
2019-03-10 01:23:12 +0100

    add glacier example

---
+++ examples/storage/glacier/main.tf
@@ -0,0 +1,14 @@
+# Create s3 glacier vault
+resource "aws_glacier_vault" "my_archive" {
+  name = "nuke-glacier"
+  }
+
+  ### Terraform modules ###
+
+  module "nuke-everything" {
+    source                         = "diodonfrost/lambda-nuke/aws"
+    name                           = "nuke-s3"
+    cloudwatch_schedule_expression = "cron(0 23 ? * FRI *)"
+    exclude_resources              = ""
+    older_than                     = "0d"
+  }
