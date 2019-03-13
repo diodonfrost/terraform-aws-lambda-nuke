5878968ad3477a3d22b49f449d60a356e806b565
diodonfrost <diodon.frost@diodonfrost.me>
2019-03-13 20:35:42 +0100

    add terraform dynamodb example

---
+++ examples/database/dynamodb/main.tf
@@ -0,0 +1,29 @@
+# Create Dynamodb table
+resource "aws_dynamodb_table" "dynamodb_nuke" {
+  name           = "dynamodb-nuke"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+}
+
+### Terraform modules ###
+
+module "nuke-everything" {
+  source                         = "diodonfrost/lambda-nuke/aws"
+  name                           = "nuke-dynamodb"
+  cloudwatch_schedule_expression = "cron(0 23 ? * FRI *)"
+  exclude_resources              = ""
+  older_than                     = "0d"
+}
