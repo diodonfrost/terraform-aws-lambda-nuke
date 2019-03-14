5ded4fa2ee48a24f5ff6c8156bd9c7f532da13eb
diodonfrost <diodon.frost@diodonfrost.me>
2019-03-14 22:26:29 +0100

    add elasticache example

---
+++ examples/database/elasticache/main.tf
@@ -0,0 +1,31 @@
+# Create memcached cluster
+resource "aws_elasticache_cluster" "nuke_memcached" {
+  cluster_id           = "memcached-nuke"
+  engine               = "memcached"
+  node_type            = "cache.t2.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.memcached1.4"
+  port                 = 11211
+}
+
+# Create redis cluster
+resource "aws_elasticache_cluster" "nuke_redis" {
+  cluster_id           = "redis-nuke"
+  engine               = "redis"
+  node_type            = "cache.t2.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis3.2"
+  engine_version       = "3.2.10"
+  port                 = 6379
+}
+
+
+### Terraform modules ###
+
+module "nuke-everything" {
+  source                         = "diodonfrost/lambda-nuke/aws"
+  name                           = "nuke-elasticache"
+  cloudwatch_schedule_expression = "cron(0 23 ? * FRI *)"
+  exclude_resources              = ""
+  older_than                     = "0d"
+}
