2f405104bda652fc12b80c1711a8c7df8b2f20db
diodonfrost <diodon.frost@diodonfrost.me>
2019-03-18 21:06:31 +0100

    add terraform network security example

---
+++ examples/network/security/main.tf
@@ -0,0 +1,52 @@
+
+# Create vpc
+resource "aws_vpc" "main" {
+  cidr_block = "10.110.0.0/16"
+}
+
+# Create security group
+resource "aws_security_group" "nuke_security_group" {
+  name        = "allow_tls_nuke"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["192.168.1.1/32"]
+  }
+}
+
+# Create network acl
+resource "aws_network_acl" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "10.3.0.0/18"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "10.3.0.0/18"
+    from_port  = 80
+    to_port    = 80
+  }
+}
+
+
+### Terraform modules ###
+
+module "nuke-everything" {
+  source                         = "diodonfrost/lambda-nuke/aws"
+  name                           = "nuke-elasticache"
+  cloudwatch_schedule_expression = "cron(0 23 ? * FRI *)"
+  exclude_resources              = ""
+  older_than                     = "0d"
+}
