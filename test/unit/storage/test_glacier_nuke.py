a606244cc37a8fcb86c280ae4d244ccb3b0a912d
diodonfrost <diodon.frost@diodonfrost.me>
2019-11-15 16:34:59 +0900

    refactoring storage unit tests

--- test/unit/storage/test_glacier_nuke.py
+++ test/unit/storage/test_glacier_nuke.py
@@ -9,15 +9,24 @@ from package.storage.glacier import NukeGlacier
 
 from .utils import create_glacier
 
+import pytest
 
+
+@pytest.mark.parametrize(
+    "aws_region, older_than_seconds, result_count",
+    [
+        ("eu-west-1", time.time() + 43200, 0),
+        ("eu-west-2", time.time() + 43200, 0),
+        ("eu-west-2", 630720000, 1),
+    ],
+)
 @mock_glacier
-def test_glacier_nuke():
+def test_glacier_nuke(aws_region, older_than_seconds, result_count):
     """Verify glacier nuke function."""
-    aws_region = "eu-west-1"
     client = boto3.client("glacier", region_name=aws_region)
 
     create_glacier(region_name=aws_region)
     glacier = NukeGlacier(aws_region)
-    glacier.nuke(older_than_seconds=time.time())
+    glacier.nuke(older_than_seconds=older_than_seconds)
     glacier_list = client.list_vaults()["VaultList"]
-    assert len(glacier_list) == 0
+    assert len(glacier_list) == result_count
--- test/unit/storage/test_s3_nuke.py
+++ test/unit/storage/test_s3_nuke.py
@@ -9,15 +9,23 @@ from package.storage.s3 import NukeS3
 
 from .utils import create_s3
 
+import pytest
 
+
+@pytest.mark.parametrize(
+    "aws_region, older_than_seconds, result_count", [
+        ("eu-west-1", time.time() + 43200, 0),
+        ("eu-west-2", time.time() + 43200, 0),
+        ("eu-west-2", 630720000, 1),
+    ]
+)
 @mock_s3
-def test_s3_nuke():
+def test_s3_nuke(aws_region, older_than_seconds, result_count):
     """Verify s3 nuke function."""
-    aws_region = "eu-west-1"
     client = boto3.client("s3", region_name=aws_region)
 
     create_s3(region_name=aws_region)
     s3 = NukeS3(aws_region)
-    s3.nuke(older_than_seconds=time.time())
+    s3.nuke(older_than_seconds=older_than_seconds)
     s3_list = client.list_buckets()["Buckets"]
-    assert len(s3_list) == 0
+    assert len(s3_list) == result_count
