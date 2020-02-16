1cfa1efc8fd3f5c104eecb77dfc0b3d555c578d1
diodonfrost <diodon.frost@diodonfrost.me>
2020-02-17 00:10:01 +0100

    update terraform examples

--- examples/test_fixture/main.tf
+++ examples/test_fixture/main.tf
@@ -1,10 +1,21 @@
 # Deploy two lambda for testing with awspec
 
-module "nuke-everything" {
-  source                         = "../.."
-  name                           = var.name
-  cloudwatch_schedule_expression = var.cloudwatch_schedule_expression
-  exclude_resources              = var.exclude_resources
-  older_than                     = var.older_than
+resource "aws_kms_key" "scheduler" {
+  description             = "test kms option on scheduler module"
+  deletion_window_in_days = 7
 }
 
+module "nuke-everything" {
+  source                         = "../.."
+  name                           = "nuke-everything" 
+  kms_key_arn                    = aws_kms_key.scheduler.arn
+  cloudwatch_schedule_expression = "cron(0 22 ? * MON-FRI *)"
+  older_than                     = "0d"
+}
+
+module "nuke-except-rds" {
+  source                         = "../.."
+  name                           = "nuke-except-rds" 
+  cloudwatch_schedule_expression = "cron(0 22 ? * MON-FRI *)"
+  older_than                     = "0d"
+}
