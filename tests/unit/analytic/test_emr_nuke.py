67011b68b0e638885f7db1a1c6ad8b63c9e6d6bf
diodonfrost <diodon.frost@diodonfrost.me>
2020-02-23 15:48:37 +0100

    add emr unit tests

---
+++ tests/unit/analytic/test_emr_nuke.py
@@ -0,0 +1,32 @@
+"""Tests for the emr nuke class."""
+
+import boto3
+import time
+
+from moto import mock_emr
+
+from package.nuke.analytic.emr import NukeEmr
+
+from .utils import create_emr
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "aws_region, older_than_seconds, result_count",
+    [
+        ("eu-west-1", time.time() + 43200, "TERMINATED"),
+        ("eu-west-2", time.time() + 43200, "TERMINATED"),
+        ("eu-west-2", 630720000, "WAITING"),
+    ],
+)
+@mock_emr
+def test_emr_nuke(aws_region, older_than_seconds, result_count):
+    """Verify emr nuke function."""
+    client = boto3.client("emr", region_name=aws_region)
+
+    create_emr(region_name=aws_region)
+    emr = NukeEmr(aws_region)
+    emr.nuke(older_than_seconds=older_than_seconds)
+    emr_list = client.list_clusters()
+    assert emr_list["Clusters"][0]["Status"]["State"] == result_count
---
+++ tests/unit/analytic/utils.py
@@ -0,0 +1,23 @@
+"""Module use by nuke unit tests."""
+
+import boto3
+
+
+def create_emr(region_name):
+    """Create emr cluster."""
+    client = boto3.client("emr", region_name=region_name)
+    az = region_name + "a"
+    client.run_job_flow(
+        Instances={
+            "InstanceCount": 3,
+            "KeepJobFlowAliveWhenNoSteps": True,
+            "MasterInstanceType": "c3.medium",
+            "Placement": {"AvailabilityZone": az},
+            "SlaveInstanceType": "c3.xlarge",
+        },
+        JobFlowRole="EMR_EC2_DefaultRole",
+        LogUri="s3://mybucket/log",
+        Name="cluster",
+        ServiceRole="EMR_DefaultRole",
+        VisibleToAllUsers=True,
+    )
