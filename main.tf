417deb70129725c6a439a91965b222d059d313a1
diodonfrost <diodon.frost@diodonfrost.me>
2021-01-01 02:42:33 +0100

    refactor(iam): use data iam_policy_document_policy instead of raw json
    Previously, the AWS IAM policy was defined with
    raw JSON format. The recommended approach to
    building AWS IAM policy documents within Terraform
    is the highly customizable aws_iam_policy_document
    data source. A short list of benefits over other
    methods include:
    * Native Terraform configuration - no need to worry about JSON formatting or syntax
    * Policy layering - create policy documents that combine and/or overwrite other policy documents
    * Built-in policy error checking
    
    This commit replaces raw json iam policy by
    erraform aws_iam_policy_document data source.

--- main.tf
+++ main.tf
@@ -5,260 +5,244 @@
 ################################################
 
 resource "aws_iam_role" "this" {
-  count       = var.custom_iam_role_arn == null ? 1 : 0
-  name        = "${var.name}-lambda-nuke"
-  description = "Allows Lambda functions to destroy all aws resources"
-
-  assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "sts:AssumeRole",
-            "Principal": {
-                "Service": "lambda.amazonaws.com"
-            },
-            "Effect": "Allow",
-            "Sid": ""
-        }
-    ]
+  count              = var.custom_iam_role_arn == null ? 1 : 0
+  name               = "${var.name}-lambda-nuke"
+  description        = "Allows Lambda functions to destroy all aws resources"
+  assume_role_policy = data.aws_iam_policy_document.this.json
 }
-EOF
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
 }
 
 resource "aws_iam_role_policy" "nuke_compute" {
-  count = var.custom_iam_role_arn == null ? 1 : 0
-  name  = "${var.name}-nuke-compute"
-  role  = aws_iam_role.this[0].id
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "ec2:DescribeInstances",
-                "ec2:TerminateInstances",
-                "ec2:DescribeSpotInstanceRequests",
-                "ec2:CancelSpotInstanceRequests",
-                "ec2:DescribeSpotFleetRequests",
-                "ec2:DeleteSpotInstanceRequest",
-                "ec2:DescribeLaunchTemplates",
-                "ec2:DeleteLaunchTemplate",
-                "ec2:DescribeSnapshots",
-                "ec2:DeleteSnapshot",
-                "ec2:DescribeVolumes",
-                "ec2:DeleteVolume",
-                "ec2:DescribeKeyPairs",
-                "ec2:DeleteKeyPair",
-                "ec2:DescribePlacementGroups",
-                "ec2:DeletePlacementGroup",
-                "ec2:DescribeImages",
-                "ec2:DeregisterImage",
-                "dlm:GetLifecyclePolicy",
-                "dlm:GetLifecyclePolicies",
-                "dlm:DeleteLifecyclePolicy",
-                "autoscaling:DescribeAutoScalingGroups",
-                "autoscaling:DeleteAutoScalingGroup",
-                "autoscaling:DescribeLaunchConfigurations",
-                "autoscaling:DeleteLaunchConfiguration",
-                "elasticloadbalancing:DescribeLoadBalancers",
-                "elasticloadbalancing:DeleteLoadBalancer",
-                "elasticloadbalancing:DescribeTargetGroups",
-                "elasticloadbalancing:DeleteTargetGroup",
-                "ecr:DescribeRepositories",
-                "ecr:DeleteRepository",
-                "eks:ListClusters",
-                "eks:DescribeCluster",
-                "eks:DeleteCluster",
-                "elasticbeanstalk:DescribeApplications",
-                "elasticbeanstalk:DescribeEnvironments",
-                "elasticbeanstalk:DeleteApplication",
-                "elasticbeanstalk:TerminateEnvironment"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
-        }
-    ]
+  count  = var.custom_iam_role_arn == null ? 1 : 0
+  name   = "${var.name}-nuke-compute"
+  role   = aws_iam_role.this[0].id
+  policy = data.aws_iam_policy_document.nuke_compute.json
 }
-EOF
+
+data "aws_iam_policy_document" "nuke_compute" {
+  statement {
+    actions = [
+      "ec2:DescribeInstances",
+      "ec2:TerminateInstances",
+      "ec2:DescribeSpotInstanceRequests",
+      "ec2:CancelSpotInstanceRequests",
+      "ec2:DescribeSpotFleetRequests",
+      "ec2:DeleteSpotInstanceRequest",
+      "ec2:DescribeLaunchTemplates",
+      "ec2:DeleteLaunchTemplate",
+      "ec2:DescribeSnapshots",
+      "ec2:DeleteSnapshot",
+      "ec2:DescribeVolumes",
+      "ec2:DeleteVolume",
+      "ec2:DescribeKeyPairs",
+      "ec2:DeleteKeyPair",
+      "ec2:DescribePlacementGroups",
+      "ec2:DeletePlacementGroup",
+      "ec2:DescribeImages",
+      "ec2:DeregisterImage",
+      "dlm:GetLifecyclePolicy",
+      "dlm:GetLifecyclePolicies",
+      "dlm:DeleteLifecyclePolicy",
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:DeleteAutoScalingGroup",
+      "autoscaling:DescribeLaunchConfigurations",
+      "autoscaling:DeleteLaunchConfiguration",
+      "elasticloadbalancing:DescribeLoadBalancers",
+      "elasticloadbalancing:DeleteLoadBalancer",
+      "elasticloadbalancing:DescribeTargetGroups",
+      "elasticloadbalancing:DeleteTargetGroup",
+      "ecr:DescribeRepositories",
+      "ecr:DeleteRepository",
+      "eks:ListClusters",
+      "eks:DescribeCluster",
+      "eks:DeleteCluster",
+      "elasticbeanstalk:DescribeApplications",
+      "elasticbeanstalk:DescribeEnvironments",
+      "elasticbeanstalk:DeleteApplication",
+      "elasticbeanstalk:TerminateEnvironment",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
 }
 
 resource "aws_iam_role_policy" "nuke_storage" {
-  count = var.custom_iam_role_arn == null ? 1 : 0
-  name  = "${var.name}-nuke-storage"
-  role  = aws_iam_role.this[0].id
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "s3:ListAllMyBuckets",
-                "s3:ListBucket",
-                "s3:ListBucketVersions",
-                "s3:GetObject",
-                "s3:GetObjectVersion",
-                "s3:DeleteObject",
-                "s3:DeleteObjectVersion",
-                "s3:DeleteBucketPolicy",
-                "s3:DeleteBucket",
-                "elasticfilesystem:DescribeFileSystems",
-                "elasticfilesystem:DeleteFileSystem",
-                "glacier:ListVaults",
-                "glacier:DescribeVault",
-                "glacier:DeleteVault"
-            ],
-            "Resource": "*",
-            "Effect": "Allow"
-        }
-    ]
+  count  = var.custom_iam_role_arn == null ? 1 : 0
+  name   = "${var.name}-nuke-storage"
+  role   = aws_iam_role.this[0].id
+  policy = data.aws_iam_policy_document.nuke_storage.json
 }
-EOF
+
+data "aws_iam_policy_document" "nuke_storage" {
+  statement {
+    actions = [
+      "s3:ListAllMyBuckets",
+      "s3:ListBucket",
+      "s3:ListBucketVersions",
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:DeleteObject",
+      "s3:DeleteObjectVersion",
+      "s3:DeleteBucketPolicy",
+      "s3:DeleteBucket",
+      "elasticfilesystem:DescribeFileSystems",
+      "elasticfilesystem:DeleteFileSystem",
+      "glacier:ListVaults",
+      "glacier:DescribeVault",
+      "glacier:DeleteVault",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
 }
 
 resource "aws_iam_role_policy" "nuke_database" {
-  count = var.custom_iam_role_arn == null ? 1 : 0
-  name  = "${var.name}-nuke-database"
-  role  = aws_iam_role.this[0].id
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "rds:DescribeDBClusters",
-                "rds:DeleteDBCluster",
-                "rds:DescribeDBInstances",
-                "rds:DeleteDBInstance",
-                "rds:DescribeDBSubnetGroups",
-                "rds:DeleteDBSubnetGroup",
-                "rds:DescribeDBClusterParameterGroups",
-                "rds:DeleteDBClusterParameterGroup",
-                "rds:DescribeDBParameterGroups",
-                "rds:DeleteDBParameterGroup",
-                "rds:DescribeDBClusterSnapshots",
-                "rds:DeleteDBClusterSnapshot",
-                "dynamodb:ListTables",
-                "dynamodb:DescribeTable",
-                "dynamodb:DeleteTable",
-                "dynamodb:ListBackups",
-                "dynamodb:DescribeBackup",
-                "dynamodb:DeleteBackup",
-                "elasticache:DescribeCacheClusters",
-                "elasticache:DeleteCacheCluster",
-                "elasticache:DescribeSnapshots",
-                "elasticache:DeleteSnapshot",
-                "elasticache:DescribeCacheSubnetGroups",
-                "elasticache:DeleteCacheSubnetGroup",
-                "elasticache:DescribeCacheParameterGroups",
-                "elasticache:DeleteCacheParameterGroup",
-                "redshift:DescribeClusters",
-                "redshift:DeleteCluster",
-                "redshift:DescribeClusterSnapshots",
-                "redshift:DeleteClusterSnapshot",
-                "redshift:DescribeClusterParameterGroups",
-                "redshift:DeleteClusterParameterGroup",
-                "redshift:DescribeClusterSubnetGroups",
-                "redshift:DeleteClusterSubnetGroup"
-            ],
-            "Effect": "Allow",
-            "Resource": "*"
-        }
-    ]
+  count  = var.custom_iam_role_arn == null ? 1 : 0
+  name   = "${var.name}-nuke-database"
+  role   = aws_iam_role.this[0].id
+  policy = data.aws_iam_policy_document.nuke_database.json
 }
-EOF
+
+data "aws_iam_policy_document" "nuke_database" {
+  statement {
+    actions = [
+      "rds:DescribeDBClusters",
+      "rds:DeleteDBCluster",
+      "rds:DescribeDBInstances",
+      "rds:DeleteDBInstance",
+      "rds:DescribeDBSubnetGroups",
+      "rds:DeleteDBSubnetGroup",
+      "rds:DescribeDBClusterParameterGroups",
+      "rds:DeleteDBClusterParameterGroup",
+      "rds:DescribeDBParameterGroups",
+      "rds:DeleteDBParameterGroup",
+      "rds:DescribeDBClusterSnapshots",
+      "rds:DeleteDBClusterSnapshot",
+      "dynamodb:ListTables",
+      "dynamodb:DescribeTable",
+      "dynamodb:DeleteTable",
+      "dynamodb:ListBackups",
+      "dynamodb:DescribeBackup",
+      "dynamodb:DeleteBackup",
+      "elasticache:DescribeCacheClusters",
+      "elasticache:DeleteCacheCluster",
+      "elasticache:DescribeSnapshots",
+      "elasticache:DeleteSnapshot",
+      "elasticache:DescribeCacheSubnetGroups",
+      "elasticache:DeleteCacheSubnetGroup",
+      "elasticache:DescribeCacheParameterGroups",
+      "elasticache:DeleteCacheParameterGroup",
+      "redshift:DescribeClusters",
+      "redshift:DeleteCluster",
+      "redshift:DescribeClusterSnapshots",
+      "redshift:DeleteClusterSnapshot",
+      "redshift:DescribeClusterParameterGroups",
+      "redshift:DeleteClusterParameterGroup",
+      "redshift:DescribeClusterSubnetGroups",
+      "redshift:DeleteClusterSubnetGroup",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
 }
 
 resource "aws_iam_role_policy" "nuke_network" {
-  count = var.custom_iam_role_arn == null ? 1 : 0
-  name  = "${var.name}-nuke-network"
-  role  = aws_iam_role.this[0].id
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "ec2:DescribeSecurityGroups",
-                "ec2:DeleteSecurityGroup",
-                "ec2:DescribeNetworkAcls",
-                "ec2:DeleteNetworkAcl",
-                "ec2:DescribeVpcEndpoints",
-                "ec2:DeleteVpcEndpoints",
-                "ec2:DescribeVpcEndpointServices",
-                "ec2:DescribeVpcEndpointServiceConfigurations",
-                "ec2:DeleteVpcEndpointServiceConfigurations",
-                "ec2:DescribeNatGateways",
-                "ec2:DeleteNatGateway",
-                "ec2:DescribeAddresses",
-                "ec2:ReleaseAddress",
-                "ec2:DescribeRouteTables",
-                "ec2:DeleteRouteTable",
-                "ec2:DescribeInternetGateways",
-                "ec2:DeleteInternetGateway",
-                "ec2:DescribeEgressOnlyInternetGateways",
-                "ec2:DeleteEgressOnlyInternetGateway",
-                "ec2:RevokeSecurityGroupEgress",
-                "ec2:RevokeSecurityGroupIngress"
-            ],
-            "Effect": "Allow",
-            "Resource": "*"
-        }
-    ]
+  count  = var.custom_iam_role_arn == null ? 1 : 0
+  name   = "${var.name}-nuke-network"
+  role   = aws_iam_role.this[0].id
+  policy = data.aws_iam_policy_document.nuke_network.json
 }
-EOF
+
+data "aws_iam_policy_document" "nuke_network" {
+  statement {
+    actions = [
+      "ec2:DescribeSecurityGroups",
+      "ec2:DeleteSecurityGroup",
+      "ec2:DescribeNetworkAcls",
+      "ec2:DeleteNetworkAcl",
+      "ec2:DescribeVpcEndpoints",
+      "ec2:DeleteVpcEndpoints",
+      "ec2:DescribeVpcEndpointServices",
+      "ec2:DescribeVpcEndpointServiceConfigurations",
+      "ec2:DeleteVpcEndpointServiceConfigurations",
+      "ec2:DescribeNatGateways",
+      "ec2:DeleteNatGateway",
+      "ec2:DescribeAddresses",
+      "ec2:ReleaseAddress",
+      "ec2:DescribeRouteTables",
+      "ec2:DeleteRouteTable",
+      "ec2:DescribeInternetGateways",
+      "ec2:DeleteInternetGateway",
+      "ec2:DescribeEgressOnlyInternetGateways",
+      "ec2:DeleteEgressOnlyInternetGateway",
+      "ec2:RevokeSecurityGroupEgress",
+      "ec2:RevokeSecurityGroupIngress",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
 }
 
 resource "aws_iam_role_policy" "nuke_analytic" {
-  count = var.custom_iam_role_arn == null ? 1 : 0
-  name  = "${var.name}-nuke-analytic"
-  role  = aws_iam_role.this[0].id
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "elasticmapreduce:ListClusters",
-                "elasticmapreduce:TerminateJobFlows",
-                "kafka:ListClusters",
-                "kafka:DeleteCluster"
-            ],
-            "Effect": "Allow",
-            "Resource": "*"
-        }
-    ]
+  count  = var.custom_iam_role_arn == null ? 1 : 0
+  name   = "${var.name}-nuke-analytic"
+  role   = aws_iam_role.this[0].id
+  policy = data.aws_iam_policy_document.nuke_analytic.json
 }
-EOF
+
+data "aws_iam_policy_document" "nuke_analytic" {
+  statement {
+    actions = [
+      "elasticmapreduce:ListClusters",
+      "elasticmapreduce:TerminateJobFlows",
+      "kafka:ListClusters",
+      "kafka:DeleteCluster",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
 }
 
 resource "aws_iam_role_policy" "nuke_monitoring" {
-  count = var.custom_iam_role_arn == null ? 1 : 0
-  name  = "${var.name}-nuke-monitoring"
-  role  = aws_iam_role.this[0].id
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": [
-                "cloudwatch:ListDashboards",
-                "cloudwatch:DeleteDashboards",
-                "cloudwatch:DescribeAlarms",
-                "cloudwatch:DeleteAlarms"
-            ],
-            "Effect": "Allow",
-            "Resource": "*"
-        }
-    ]
+  count  = var.custom_iam_role_arn == null ? 1 : 0
+  name   = "${var.name}-nuke-monitoring"
+  role   = aws_iam_role.this[0].id
+  policy = data.aws_iam_policy_document.nuke_monitoring.json
 }
-EOF
+
+data "aws_iam_policy_document" "nuke_monitoring" {
+  statement {
+    actions = [
+      "cloudwatch:ListDashboards",
+      "cloudwatch:DeleteDashboards",
+      "cloudwatch:DescribeAlarms",
+      "cloudwatch:DeleteAlarms",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
 }
 
 locals {
