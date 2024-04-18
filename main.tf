bfdd938eb5db45534b91c3e00c4917cfa3be7653
emanjoh1 <112668324+emanjoh1@users.noreply.github.com>
2024-04-17 21:15:16 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -310,7 +310,7 @@ resource "aws_lambda_function" "this" {
   role             = var.custom_iam_role_arn == null ? aws_iam_role.this[0].arn : var.custom_iam_role_arn
   handler          = "nuke.main.lambda_handler"
   source_code_hash = data.archive_file.this.output_base64sha256
-  runtime          = "python3.7"
+  runtime          = "python3.12"
   timeout          = "900"
   kms_key_arn      = var.kms_key_arn == null ? "" : var.kms_key_arn
   tags             = var.tags
