ffabf0568d47568ea6cd013665aaa264281fd93d
diodonfrost <diodon.frost@diodonfrost.me>
2025-05-03 03:09:54 +0200

    refactor(test_fixture): remove unused KMS key
    Removed the unused AWS KMS key resource from the
    test fixture and updated the CloudWatch schedule
    expression for the nuke-everything module to run
    at 6 AM daily.

--- examples/test_fixture/main.tf
+++ examples/test_fixture/main.tf
@@ -1,21 +1,8 @@
 # Deploy two lambda for testing with awspec
 
-resource "aws_kms_key" "scheduler" {
-  description             = "test kms option on scheduler module"
-  deletion_window_in_days = 7
-}
-
 module "nuke-everything" {
   source                         = "../.."
   name                           = "nuke-everything"
-  kms_key_arn                    = aws_kms_key.scheduler.arn
-  cloudwatch_schedule_expression = "cron(0 22 ? * MON-FRI *)"
-  older_than                     = "0d"
-}
-
-module "nuke-except-rds" {
-  source                         = "../.."
-  name                           = "nuke-except-rds"
-  cloudwatch_schedule_expression = "cron(0 22 ? * MON-FRI *)"
+  cloudwatch_schedule_expression = "cron(0 6 ? * * *)"
   older_than                     = "0d"
 }
